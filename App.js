4d350d3cb4b8ee1bdb98ab81317e9e5640ced7f7
Vladimir <vladimir.evstratov@list.ru>
2020-12-01 14:17:28 +0300

    3 lesson

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      <SafeAreaView style={{flex: 1}}>
         <Main />
       </SafeAreaView>
     </>
--- src/Components/CircleButton/index.js
+++ src/Components/CircleButton/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
 
-const CircleButton = () => {
+const CircleButton = ({add}) => {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={() => add()}>
       <Text style={styles.text}>+</Text>
     </TouchableOpacity>
   );
